752966dd8b3f9af4efa5c83c66e272af251ae96c
leeburgess67 <leeburgessmail@gmail.com>
2018-06-15 21:09:12 +0100

    DB created

---
+++ db/inventory_app.sql
@@ -0,0 +1,20 @@
+DROP TABLE products;
+DROP TABLE suppliers:
+
+
+CREATE TABLE suppliers (
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  email VARCHAR(255)
+);
+
+CREATE TABLE products (
+  id SERIAL4 PRIMARY KEY,
+  description VARCHAR(255),
+  cost_price INT4,
+  retail_price INT4,
+  item_markup INT4,
+  bookstock INT2,
+  supplier_id INT4 REFERENCES suppliers(id)
+
+);
