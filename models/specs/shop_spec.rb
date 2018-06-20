4ca0748ac066d3524b7a23c3ac8f46dd3b2f3f78
leeburgess67 <leeburgessmail@gmail.com>
2018-06-20 17:17:43 +0100

    refactored shop spec

--- models/specs/shop_spec.rb
+++ models/specs/shop_spec.rb
@@ -8,37 +8,49 @@ require_relative("../shop.rb")
 class TestShop < MiniTest::Test
 
   def setup
+    options = ({
+      "name" => "Fender UK",
+      "email" => "sales@fender.co.uk",
+      "phone" => "01311346542"
+    })
+
+    @supplier1 = Supplier.new(options)
 
     options = ({
-      "name" => "Cool Beanz Co",
-      "email" => "coolbeanz@unilever.com",
-      'id' => 1
-      })
-      @supplier1 = Supplier.new(options)
+      "name" => "Gibson",
+      "email" => "sales@gibson.com",
+      "phone" => "01414927593"
+    })
 
-  options = ({
-    "description" => "Heinz Baked Beans",
-      "cost_price" => 45,
-      "retail_price" => 75,
-      "bookstock" => 25,
-      "supplier_id" => @supplier1.id
+    @supplier2 = Supplier.new(options)
+
+
+    options = ({
+      "description" => "Gibson Les Paul Standard",
+      "cost_price" => 1000,
+      "retail_price" => 1500,
+      "bookstock" => 3,
+      "supplier_id" => @supplier2.id,
+      "upn" => 50321
       })
 
       @product1 = Product.new(options)
-  options = ({
-    "description" => "Heinz",
-      "cost_price" => 33,
-      "retail_price" => 75,
-      "bookstock" => 25,
-      "supplier_id" => @supplier1.id
-      })
 
-      @product2 = Product.new(options )
+      options = ({
+        "description" => "Fender Telecaster Deluxe",
+        "cost_price" => 350,
+        "retail_price" => 550,
+        "bookstock" => 1,
+        "supplier_id" => @supplier1.id,
+        "upn" => 50847
+        })
+
+        @product2 = Product.new(options)
 
       items = [@product1, @product2]
-  @shop = Shop.new(items)
-  end
+      @shop = Shop.new(items)
 
+end
   def test_shop_has_items
     result = @shop.items()
     assert_equal([@product1, @product2], result)
